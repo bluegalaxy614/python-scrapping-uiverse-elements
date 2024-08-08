a3c9005ab8f7b45a901774b0d45e56af26da8455
Mercury614 <starandrii170@gmail.com>
2024-08-09 03:25:51 +0900

    update : to onefile

--- main.py
+++ main.py
@@ -1,17 +1,75 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from pymongo import MongoClient
-
-# from webdriver_manager.chrome import ChromeDriverManager
-
 from dotenv import load_dotenv
 import os
 
-from scraper import scraper
+def store_data_to_db(db_collection, document):
+    db_collection.insert_one(document)
+    
+def store_data_by_link(db_collection, driver, link, category, tag, style):
+    driver.get(link)
+    WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.TAG_NAME, "main"))
+    )
+    
+    str_for_html = driver.find_elements(By.TAG_NAME, "textarea")[0].text
+    str_for_css = driver.find_elements(By.TAG_NAME, "style")[0].text
+    
+    element_for_views = driver.find_elements(By.CSS_SELECTOR, "div.flex.items-center.pl-3")
+    views_text = element_for_views[0].text
+    number_views = int(views_text.split()[0])
+    
+    element_for_tags = driver.find_elements(By.TAG_NAME, "aside")
+    elements_for_tag = element_for_tags.find_elements(By.TAG_NAME, "a")
+    
+    tags = []
+    for tag_element in elements_for_tag:
+        tags.append(tag_element.text)
+        
+    report_date = element_for_tags.find_elements(By.CSS_SELECTOR, "div.flex.items-center.gap-3.font-normal.text-gray-400")[0].text
+    
+    document = {
+        "link": link,
+        "html": str_for_html,
+        "css": str_for_css,
+        "views": number_views,
+        "category": category,
+        "tag": tag,
+        "style": style,
+        "tags": tags,
+        "report_date": report_date
+    }
+    
+    print(document)
+    
+    store_data_to_db(db_collection, document)
+    
+def scraper(db_collection, driver, origin_url, category, tag, style, page):
+    url = f"{origin_url}/{category}?t={style}&tags={tag}&page={page}"
+    driver.get(url)
+    WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.TAG_NAME, "section"))
+    )
+    
+    all_articles = driver.find_elements(By.TAG_NAME, "article")
+    
+    for article in all_articles:
+        links = article.find_elements(By.TAG_NAME, "a")
+        article_links = [link.get_attribute("href") for link in links]
+        for link in article_links:
+            print(link)
+            store_data_by_link(db_collection, driver, link, category, tag, style)
+        
+    if(all_articles == []):
+        return False
+    return True
 
-# Load environment variables from .env file
+
 load_dotenv()
-
 origin_url = os.getenv("TARGET_URL")
 
 db_url = os.getenv("DB_URL")
@@ -24,7 +82,6 @@ db_collection = db[db_collection]
 
 chrome_driver_path = os.getenv("CHROME_DRIVER_PATH")
 
-# Check if the URL is loaded successfully
 if not origin_url:
     raise ValueError("TARGET_URL is not set in the .env file")
 
@@ -33,23 +90,19 @@ print(f"Navigating to URL: {origin_url}")
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service)
 
-# Call the scraper
-url_for_button = f"{origin_url}/buttons"
-
+categories = ["buttons", "checkboxes", "switches", "cards", "loaders","inputs", "radio-buttons", "forms", "patterns", "tooltips"]
 tags = ["neumorphism", "3d", "gradient"]
 styles = ["tailwind", "css"]
 
-for tag in tags:
-    url_for_tag = f"{url_for_button}?tags={tag}"
-    for style in styles:
-        url_for_style = f"{url_for_tag}?t={style}"
-        page = 0
-        while True:
-            next_page_url = f"{url_for_tag}&page={page}"
-            is_not_final_page = scraper(db_collection, driver, next_page_url)
-            if( is_not_final_page == False ):
-                break
-            page += 1
+for category in categories:
+    for tag in tags:
+        for style in styles:
+            page = 0
+            while True:
+                is_not_final_page = scraper(db_collection, driver, origin_url, category, tag, style, page)
+                if( is_not_final_page == False ):
+                    break
+                page += 1
 
 driver.quit()
 client.close()
--- scraper.py
+++ scraper.py
@@ -3,17 +3,52 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-from mongo import store_data_to_db
-
-def store_data_by_link(db_collection, driver, link):
+def store_data_to_db(db_collection, document):
+    db_collection.insert_one(document)
+    
+def store_data_by_link(db_collection, driver, link, category, tag, style):
     driver.get(link)
     WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.TAG_NAME, "main"))
     )
     
-    # store_data_to_db(db_collection, document)
-def scraper(db_collection, driver, url):
-
+    str_for_html = driver.find_elements(By.TAG_NAME, "textarea")[0].text
+    str_for_css = driver.find_elements(By.TAG_NAME, "style")[0].text
+    
+    element_for_views = driver.find_elements(By.CSS_SELECTOR, "div.flex.items-center.pl-3")
+    views_text = element_for_views[0].text
+    number_views = int(views_text.split()[0])
+    
+    element_for_tags = driver.find_elements(By.TAG_NAME, "aside")
+    
+    tag_name = element_for_tags.find_elements(By.TAG_NAME, "h2")[0].text
+    elements_for_tag = element_for_tags.find_elements(By.TAG_NAME, "a")
+    
+    tags = []
+    for tag_element in elements_for_tag:
+        tags.append(tag_element.text)
+        
+    report_date = element_for_tags.find_elements(By.CSS_SELECTOR, "div.flex.items-center.gap-3.font-normal.text-gray-400")[0].text
+    
+    document = {
+        "link": link,
+        "html": str_for_html,
+        "css": str_for_css,
+        "views": number_views,
+        "category": category,
+        "tag": tag,
+        "tag_name": tag_name,
+        "style": style,
+        "tags": tags,
+        "report_date": report_date
+    }
+    
+    print(document)
+    
+    store_data_to_db(db_collection, document)
+    
+def scraper(db_collection, driver, origin_url, category, tag, style, page):
+    url = f"{origin_url}/{category}?t={style}&tags={tag}&page={page}"
     driver.get(url)
     WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.TAG_NAME, "section"))
@@ -25,7 +60,8 @@ def scraper(db_collection, driver, url):
         links = article.find_elements(By.TAG_NAME, "a")
         article_links = [link.get_attribute("href") for link in links]
         for link in article_links:
-            store_data_by_link(db_collection, driver, link)
+            print(link)
+            store_data_by_link(db_collection, driver, link, category, tag, style)
         
     if(all_articles == []):
         return False
